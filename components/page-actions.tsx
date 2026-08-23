f0a4d636207fbee79af0ca1153e62458fcdb94b6
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-08-23 22:35:50 +0200

    fix

--- components/page-actions.tsx
+++ components/page-actions.tsx
@@ -1,10 +1,10 @@
-import { KofiIcon } from '@/components/ui/icons';
+import { KofiLogo } from '@/components/ui/icons';
 import { kofiUrl } from '@/lib/shared';
 
 export function KofiButton() {
   return (
     <a href={kofiUrl} target="_blank" rel="noreferrer noopener" aria-label="Ko-fi">
-      <KofiIcon className="h-6 w-auto" />
+      <KofiLogo className="h-6 w-auto" />
     </a>
   );
 }
